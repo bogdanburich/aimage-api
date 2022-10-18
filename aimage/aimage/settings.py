3c4797f2b906a7243bd5643480c76c41c5f642ee
Bogdan <bogdan.buritch@yandex.ru>
2022-10-18 23:13:35 +0400

    Add CI/CD

--- aimage/aimage/settings.py
+++ aimage/aimage/settings.py
@@ -81,12 +81,12 @@ if ENVIRONMENT == 'DEV':
 else:
     DATABASES = {
         'default': {
-            'DB_ENGINE': os.getenv('DB_ENGINE'),
-            'DB_NAME': os.getenv('DB_NAME'),
-            'DB_USER': os.getenv('DB_USER'),
-            'DB_PASSWORD': os.getenv('DB_PASSWORD'),
-            'DB_HOST': os.getenv('DB_HOST'),
-            'DB_PORT': os.getenv('DB_PORT'),
+            'ENGINE': os.getenv('DB_ENGINE'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
         }
     }
 
